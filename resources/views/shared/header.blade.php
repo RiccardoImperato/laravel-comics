083e3490fea6fb0542c301edbed8d58dbd440075
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-26 17:04:51 +0100

    feat: header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,5 +1,41 @@
 {{-- Header --}}
 <header>
-    <h3>header</h3>
+    <div class="container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <ul>
+            <li>
+                <a href="#">Characters</a>
+            </li>
+            <li>
+                <a href="#">Comics</a>
+            </li>
+            <li>
+                <a href="#">Movies</a>
+            </li>
+            <li>
+                <a href="#">Tv</a>
+            </li>
+            <li>
+                <a href="#">Games</a>
+            </li>
+            <li>
+                <a href="#">Collectibles</a>
+            </li>
+            <li>
+                <a href="#">Videos</a>
+            </li>
+            <li>
+                <a href="#">Fans</a>
+            </li>
+            <li>
+                <a href="#">News</a>
+            </li>
+            <li>
+                <a href="#">Shop</a>
+            </li>
+        </ul>
+    </div>
 </header>
 {{-- Header --}}
