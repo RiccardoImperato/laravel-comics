025c54a79e4585451626599004db71324cbbea2e
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-28 13:29:09 +0100

    feat: badge current series e bottone load more

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,21 @@
 
 @section('main')
     <main>
-        <ul class="container comics-list">
-            @foreach ($comics as $comic)
-                <li class="card">
-                    <div>
-                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                    </div>
-                    <div class="title">{{ $comic['title'] }}</div>
-
-                </li>
-            @endforeach
-        </ul>
+        <div class="container">
+            <div class="series badge">Current series</div>
+            <ul class="comics-list">
+                @foreach ($comics as $comic)
+                    <li class="card">
+                        <div>
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <a href="about" class="title">{{ $comic['title'] }}</a>
+                    </li>
+                @endforeach
+            </ul>
+            <div class="more">
+                <button class="badge">Load more</button>
+            </div>
+        </div>
     </main>
 @endsection
